9dff5f2d7470f7f1afd8e7bf3fb884db547b1ccd
Adimas Sastra Nugraha <dimas.dev31@gmail.com>
2021-02-01 09:52:05 +0700

    [ADD] added csrf_token metak tag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
 	<title>Laravel + ReactJS</title>
 	<!-- import css -->
 	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 
